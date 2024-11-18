243525ca027181113af67d3ada972427d598c635
T3hCyberRat <t3hcyberrat@gmail.com>
2024-11-18 21:16:12 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "./home.css";
+import Logo from "./assets/logo.png";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-12 col-md-6 offset-md-3 text-center px-0 mh-400">
+          <img src={Logo.src} className="logo-img" />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="col-sm-12 col-md-6 offset-md-3 text-center">
+          <p className="invite-title fs-1 lh-sm mb-0">Welcome 127.0.0.1</p>
+        </div>
+
+        <div className="col-sm-12 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+          <p className="mb-0">T3hCyberRat, .connection & SOC studio are throwing a holiday bash and YOU'RE INVITED!</p>
+
+          <p>
+            Join us for an evening of merriment, music, and making memories (or just plain old having fun).
+          </p>
+
+          <p className="important-shit">Date: [Insert Date]</p>
+          <p className="important-shit">Time: [Insert Time]</p>
+          <p className="important-shit">Location: [Insert Location]</p>
+
+          <p className="mb-0">Dress code: Festive vibes only (hoodies, sweatpants, and holiday cheer encouraged)</p>
+          <p className="mb-0">See you there, minions!</p>
+        </div>
+
+        {/* <div className="col-12">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email address</label>
+              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+
+            <button type="submit" className="btn btn-link submit-btn">Submit</button>
+          </form>
+        </div> */}
+
+        <div className="col-sm-12 col-md-6 offset-md-3 align-self-end text-center">
+          <p>// from T3hCyberRat with l0v3</p>
+        </div>
+      </div>
     </div>
   );
 }
